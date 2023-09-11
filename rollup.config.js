d058c0e724bfdcc6e4886d80bb3fea74add8652b
Haider959 <saochiha@gmail.com>
2023-09-11 16:42:53 +0300

    init

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from "rollup-plugin-typescript2";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import packageJson from "./package.json";
+
+const config = {
+  input: "src/index.ts",
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      exclude: ["**/*.stories.tsx", "**/*.stories.mdx"],
+    }),
+    postcss({
+      plugins: [],
+      minimize: true,
+    }),
+    terser(),
+  ],
+  output: {
+    file: packageJson.main,
+    format: "cjs",
+    sourcemap: false,
+  },
+};
+
+export default config;
